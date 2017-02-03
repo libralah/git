f2e377641db1852130198d03d88fd6057dc32e56
libralah <hungnguyen@Hungs-Mac-mini.fios-router.home>
2017-02-03 05:48:42 -0500

    Adding something

--- Git/ViewController.swift
+++ Git/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("aw yeah")
         print("someting new")
+        print("hello there")
     }
 
     override func didReceiveMemoryWarning() {
